1cf36c7c6377787116e8277ce5ef1acc4b8c7a7a
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-06 22:20:09 +0530

    Program to compare two Strings and check that they are Anagram or not

--- src/Frequency/Anagram.java
+++ src/Frequency/Anagram.java
@@ -1,52 +1,27 @@
 package Frequency;
 
+import java.util.Arrays;
+
 public class Anagram {
-    private char[] String1;
-    private char[] String2;
-    private int N;
+    private String String1;
+    private String String2;
 
     public Anagram(String string1, String string2) {
-        String1 = string1.toCharArray();
-        String2 = string2.toCharArray();
-        N = String1.length;
+        char[] temp = string1.toCharArray();
+        Arrays.sort(temp);
+        String1 = new String(temp);
+        temp = string2.toCharArray();
+        Arrays.sort(temp);
+        String2 = new String(temp);
     }
 
     public boolean check(){
-        if (String1.length==String2.length) {
-            sort();
-            for (int i = 0; i < N; i++)
-                if (String1[i] != String2[i])
-                    return false;
+        if (String1.length()==String2.length()) {
+                if (String1.equals(String2))
+                    return true;
+                else return false;
         }
         else return false;
-        return true;
-    }
-
-    private void sort(){
-        for (int i = 0; i < N; i++)
-        {
-            // Find the minimum element in unsorted array
-            int small = i;
-            for (int j = i+1; j < N; j++)
-                if ((int)String1[j] <  (int)String1[small])
-                    small = j;
-            // Swap the found minimum element with the first element
-            char temp = String1[small];
-            String1[small] = String1[i];
-            String1[i] = temp;
-        }
-        for (int i = 0; i < N; i++)
-        {
-            // Find the minimum element in unsorted array
-            int small = i;
-            for (int j = i+1; j < N; j++)
-                if ((int)String2[j] <  (int)String2[small])
-                    small = j;
-            // Swap the found minimum element with the first element
-            char temp = String2[small];
-            String2[small] = String2[i];
-            String2[i] = temp;
-        }
     }
 
     //Driver code

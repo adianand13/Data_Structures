ae045c5f213010b745e9e2b12f7787d7d8d2565d
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-10 23:01:09 +0530

    Node Structure for doubly linked list

---
+++ src/DoublyLinkedList/Node.java
@@ -0,0 +1,39 @@
+package DoublyLinkedList;
+
+public class Node {
+    private int Value;
+    private Node Next;
+    private  Node Previous;
+
+    public Node(int value) {
+        Value = value;
+        Next = null;
+    }
+
+    public Node(int value, Node next) {
+        Value = value;
+        Next = next;
+    }
+
+    public Node(int value, Node next, Node previous) {
+        Value = value;
+        Next = next;
+        Previous = previous;
+    }
+
+    public int getValue() { return Value; }
+
+    public Node getNext() { return Next; }
+
+    public Node getPrevious() { return Previous; }
+
+    public boolean hasNext(){ return (Next!=null); }
+
+    public boolean hasPrevious(){ return (Previous!=null); }
+
+    public void setValue(int value) { Value = value; }
+
+    public void setNext(Node next) { Next = next; }
+
+    public void setPrevious(Node previous) { Previous = previous; }
+}

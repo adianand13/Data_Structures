11e939e8a0e1e1e1bc94d560a4ccf829d494e87b
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-13 23:14:47 +0530

    tree node

---
+++ src/Tree/Node.java
@@ -0,0 +1,29 @@
+package Tree;
+
+public class Node {
+    private int value;
+    private Node Left;
+    private Node Right;
+
+    public Node(int value) {
+        this.value = value;
+    }
+
+    public Node(int value, Node left, Node right) {
+        this.value = value;
+        Left = left;
+        Right = right;
+    }
+
+    public int getValue() { return value; }
+
+    public void setValue(int value) { this.value = value; }
+
+    public Node getLeft() { return Left; }
+
+    public void setLeft(Node left) { Left = left; }
+
+    public Node getRight() { return Right; }
+
+    public void setRight(Node right) { Right = right; }
+}

e7dbb48a7ba1781a724629852f8586cf87e78a03
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-14 15:18:48 +0530

    Tree Traversal Inorder

--- src/Tree/InOrder.java
+++ src/Tree/InOrder.java
@@ -1,4 +1,21 @@
 package Tree;
 
+import BinarySearchTree.*;
+
 public class InOrder {
+
+    public void traverse(Node node){
+        if (node.hasLeft()) traverse(node.getLeft());
+        System.out.print(" "+node.getValue());
+        if (node.hasRight()) traverse(node.getRight());
+    }
+
+    //test code
+    public static void main(String[] args) {
+        int[] values = {10,6,15,3,8,20};
+        InOrder inOrder = new InOrder();
+        BinarySearchTree binarySearchTree = new BinarySearchTree();
+        binarySearchTree.insertArray(values);
+        inOrder.traverse(binarySearchTree.getRoot());
+    }
 }

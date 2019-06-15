07d241847861878e72de7e3347a80a71349556ab
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-15 23:27:24 +0530

    Max Binary Heap implementation

--- src/Heap/MaxBinaryHeap.java
+++ src/Heap/MaxBinaryHeap.java
@@ -9,10 +9,19 @@ public class MaxBinaryHeap {
     public void insert(int element){
         List.push(element);
         bubbleup();
-
     }
 
-    public void bubbleup(){
-
+    private void bubbleup(){
+        int index = List.size()-1;
+        int element = List.get(index);
+        while (true){
+            int parentIndex = (index-1)/2;
+            int parent = List.get(parentIndex);
+            if (element <= parent)
+                break;
+            List.set(parentIndex,element);
+            List.set(index,parent);
+            index = parentIndex;
+        }
     }
 }

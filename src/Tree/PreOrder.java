6a916c3ba009ecc48976e1f565862585f05a9800
Aditya Anand <anand.aditya.anand@gmail.com>
2019-06-16 22:18:31 +0530

    Tree traversal PreOrder

--- src/Tree/PreOrder.java
+++ src/Tree/PreOrder.java
@@ -1,4 +1,12 @@
 package Tree;
 
+import BinarySearchTree.*;
+
 public class PreOrder {
+
+    public void traverse(Node node){
+        System.out.print(" "+node.getValue());
+        if (node.hasLeft()) traverse(node.getLeft());
+        if (node.hasRight()) traverse(node.getRight());
+    }
 }
